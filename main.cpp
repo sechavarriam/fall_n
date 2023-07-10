018e4a579bdcaff5f14ed6992c9b8c8be06b042b
Sebastián Echavarría <sechavarriam@unal.edu.co>
2023-07-10 18:17:30 -0500

    typedef Mat2D added in headder_files.h

--- main.cpp
+++ main.cpp
@@ -7,11 +7,19 @@ int main(){
 
     Node N1 = Node(1, 0.0,0.0,0.0);
 
-    int D1;
-    int D2;
-    Matrix<double,2> M(D1,D2);
+    int D1 =2 ;
+    int D2 =2 ;
 
-    std::cout << M << "\n"; 
+    double data[2][2]{{1,2},{3,4}};
+
+    Matrix<double,2> M1({{1,2},{2,4}});
+    Matrix<double,2> M2({{1,1},{1,1}});
+
+    Matrix<double,2> M3(data);
+
+    Mat2D M4(data);
+
+    std::cout << M4 << "\n"; 
 
     
 };
