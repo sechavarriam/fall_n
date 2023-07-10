e74881ce59bdc00e5df39d8e86ea2819fb6c01b2
Sebastián Echavarría <sechavarriam@unal.edu.co>
2023-07-10 13:38:52 -0500

    Small Matrix Class added. Taken from Stroustup text books

--- header_files.h
+++ header_files.h
@@ -6,16 +6,23 @@
 #include <math.h>   // Header <cmath> declares a set of functions to compute common mathematical operations and transformations.
 #include <string>   // Strings are objects that represent sequences of characters.
 
+#include <algorithm>// Defines a collection of functions especially designed to be used on ranges of elements (e.g. std::find).
+                    // Usado en Matrix.h
+
 
 //#include <numeric>  // This header describes a set of algorithms to perform certain operations on sequences of numeric values.
-//#include "dumb_math.h"
+
+
+
+
+#include "src/numeric_utils/Matrix.h"
+
 
 #include "src/domain/Node.h"
 #include "src/domain/elements/Element.h"
 #include "src/domain/elements/ContinuumElement.h"
 
 /*---------------------------
-#include <algorithm>// Defines a collection of functions especially designed to be used on ranges of elements (e.g. std::find).
 #include <fstream>  // Header providing file stream classe (Read and write files).
 #include <iomanip>
 #include <memory>   // This header defines general utilities to manage dynamic memory   
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 
 #include"header_files.h"
+#include "src/numeric_utils/Matrix.h"
+
 
 int main(){
 
     Node N1 = Node(1, 0.0,0.0,0.0);
+
+    Numeric_lib::Matrix<double,2> M;
+    
     
 };
