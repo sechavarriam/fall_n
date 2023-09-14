f2437985f1fe6e72d54064e90edc9b0e9bc90471
Sebastián Echavarría <sechavarriam@unal.edu.co>
2023-09-14 15:15:19 -0500

    Perfec Forwarding constructor to Tensor.  Composition instead Inheritance from Eigen.

--- src/numerics/Tensor.h
+++ src/numerics/Tensor.h
@@ -1,8 +1,8 @@
 #ifndef FN_TENSOR
 #define FN_TENSOR
 
-#
 
+#include <utility>
 #include "../domain/Topology.h"
 
 #include <Eigen/Dense>
@@ -25,29 +25,36 @@ consteval ushort Voigth_Dim1(){
 
 template<ushort Dim,  ushort Order> requires Topology::EmbeddableInSpace<Dim>
 consteval ushort Voigth_Dim2(){
-    if constexpr (Dim==1){
+    if constexpr (Order==1){
         return 1; 
-    }else if constexpr(Dim==2){
+    }else if constexpr(Order==2){
         return 1;
-    }else if constexpr(Dim==3){
+    }else if constexpr(Order==3){
         return 6;
     };
 };
 
+
+template<ushort Dim,  ushort Order> 
+using VoightTensorContainer 
+    = Eigen::Matrix<double, Voigth_Dim1<Dim,Order>(), Voigth_Dim2<Dim,Order>()> ;
 // ========================================================================
 // Voight notation tensor second order tensor (Vector Form)
 
-template<ushort Dim,  ushort Order> 
-using VoightTensorContainer = Eigen::Matrix<double, Voigth_Dim1<Dim,Order>(), Voigth_Dim2<Dim,Order>()> ;
+
 
 template<ushort Dim,  ushort Order> 
-class Tensor : VoightTensorContainer<Dim, Order>{
+class Tensor{
   private:
-    
+
+    VoightTensorContainer<Dim, Order> components_;
     // More things.
 
   public:
-    template <typename... Args> Tensor(Args... args) : VoightTensorContainer<Dim, Order>(args...) {}
+    template <typename... Args> Tensor(Args&&... args):
+    components_{VoightTensorContainer<Dim, Order>(std::forward<Args>(args)...)}
+    {}
+
     ~Tensor(){};
 };
 
