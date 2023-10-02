36d8767087df11f027995dda89f7ff8510d6ed12
Sebastián Echavarría <sechavarriam@unal.edu.co>
2023-10-02 13:09:27 -0500

    Generic Interpolant functor outlined.

--- src/numerics/InterpolationFunction.h
+++ src/numerics/InterpolationFunction.h
@@ -1,23 +1,48 @@
 #ifndef FN_ABSTRACT_INTERPOLATION_FUNCTION
 #define FN_ABSTRACT_INTERPOLATION_FUNCTION
 
+#include <array>
 #include <functional>
 #include "../domain/Point.h"
 
 
-template <typename ReturnType, unsigned short Dim, unsigned short nPoints>
-class InterpolationFunction{
+
+
+template <unsigned short Dim, 
+          unsigned short nPoints, 
+          typename       PointType,
+          typename       DataType  = double, 
+          typename       ReturnType= double>
+class Interpolant{ // Interpolation Function
 
   private:
+    std::function<ReturnType(PointType)> F_; //Implemented in derived classes (e.g. Element Shape Functions).
 
-    std::function<ReturnType(Point<Dim>)> function_;
-
+    // For optimazation purposes, the points and values shouln't be stored in the interpolant.    
     //std::array<Point<Dim>,nPoints> points_;
     //std::array<ReturnType,nPoints> values_;
+  public:      
+    ReturnType operator()(PointType x) const {return F_(x);};
 
-  public:  
-    InterpolationFunction(){};
-    virtual ~InterpolationFunction(){};
+    //Constructors
+
+    Interpolant(const std::function<ReturnType(PointType)>& F){
+      F_ = F; // Copy F 
+    };
+
+    Interpolant(std::array<PointType,nPoints> SampleNodes, std::array<DataType,nPoints> SampleValues){
+      // Construct F_ from array of nodes and values.
+    };
+
+    Interpolant(const PointType& SampleNodes, const DataType& SampleValues){
+      // Construct F_ from SampleNode and SampleValue references (could be the nodes and values of an element)
+    };
+    
+    Interpolant(PointType&& SampleNodes, DataType&& SampleValues){
+      // Construct F_ forwarding SampleNodes and SampleValues
+    };
+
+    virtual ~Interpolant(){};
 };
 
 
