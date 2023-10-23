86df17b69755edeaa889a30db5e9e6330d6bada9
Sebastián Echavarría <sechavarriam@unal.edu.co>
2023-10-23 11:10:57 -0500

    NonOwning Element partially implemented

--- main.cpp
+++ main.cpp
@@ -43,16 +43,16 @@ int main(){
     D.add_node( Node<dim>(8, 0.0, 1.0, 1.0) );
     D.add_node( Node<dim>(9, 0.5, 0.5, 0.5) );
 
-    D.make_element<ElementBase<dim,8 ,42> > (1 , {1,2,3,4,5,6,7,8});   
-    D.make_element<ElementBase<dim,3 ,42> > (2 , {1,2,3});
-    D.make_element<ElementBase<dim,5 ,42> > (4 , {1,2,3,4,5});
-    D.make_element<ElementBase<dim,7 ,42> > (5 , {1,2,3,4,5,6,7});
-    D.make_element<ElementBase<dim,14   > > (7 , {1,2,3,4,5,6,7,8,9,10,11,12,13,14});
-    D.make_element<ElementBase<dim,5    > > (16, {1,2,3,4,5});
-    D.make_element<ContinuumElement<dim,9>> (2 , {1,2,3,4,5,6,7,8,9});
-    D.make_element<ElementBase<dim,5    > > (42, {1,2,3,4,5}); 
-    D.make_element<ElementBase<dim,9    > > (44, {1,2,3,4,5,6,7,8,9}); 
-    D.make_element<ElementBase<dim,13   > > (50, {1,2,3,4,5,6,7,8,9,10,11,12,13});
+    //+D.make_element<ElementBase<dim,8 ,42> > (1 , {1,2,3,4,5,6,7,8});   
+    //+D.make_element<ElementBase<dim,3 ,42> > (2 , {1,2,3});
+    //+D.make_element<ElementBase<dim,5 ,42> > (4 , {1,2,3,4,5});
+    //+D.make_element<ElementBase<dim,7 ,42> > (5 , {1,2,3,4,5,6,7});
+    //+D.make_element<ElementBase<dim,14   > > (7 , {1,2,3,4,5,6,7,8,9,10,11,12,13,14});
+    //+D.make_element<ElementBase<dim,5    > > (16, {1,2,3,4,5});
+    //+D.make_element<ContinuumElement<dim,9>> (2 , {1,2,3,4,5,6,7,8,9});
+    //+D.make_element<ElementBase<dim,5    > > (42, {1,2,3,4,5}); 
+    //+D.make_element<ElementBase<dim,9    > > (44, {1,2,3,4,5,6,7,8,9}); 
+    //+D.make_element<ElementBase<dim,13   > > (50, {1,2,3,4,5,6,7,8,9,10,11,12,13});
                         
     //for (auto const& e: D.elements_){
     //    std::cout << id(e)<< " " << num_nodes(e)<< " " << num_dof(e)<< " ";
