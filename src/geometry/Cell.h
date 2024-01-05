f5733d0bc1df16f65ada09f4ba90837bc38cd38b
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-01-04 21:44:31 -0500

    CMake 2 C++23. Refactoring cell in process

--- src/geometry/Cell.h
+++ src/geometry/Cell.h
@@ -25,7 +25,7 @@ template <ushort Dim, ushort... num_node_per_direction>
 static consteval std::size_t n_nodes_() {
     auto constexpr n_params = sizeof...(num_node_per_direction);
     if constexpr (n_params == 1) {
-      auto constexpr n = ((num_node_per_direction) * ...);
+      auto constexpr n = ((num_node_per_direction) * ...); // Es necesario reducirlo así sea solo un parametro.
       if constexpr (Dim == 0) {
         return 1;
       } 
@@ -47,50 +47,82 @@ static consteval std::size_t n_nodes_() {
     };
 }
 
-template <ushort n> static inline constexpr double delta_i() {
-  if constexpr (n == 1)
+
+static inline constexpr double delta_i(int n ) {
+  constexpr double interval_size = 2.0;
+  if (n == 1)
     return 0.0;
-  return 2.0 / (n - 1);
+  return interval_size / (n - 1);
 }; // Interval size per direction i.
 
-template <ushort n> static inline constexpr double xi(auto i) {
-  return -1.0 + i*delta_i<n>();
-}; // Coordinate of the i-th node in the reference cell.
+//template <ushort n> 
+//static inline constexpr double xi(auto i) {
+//  return -1.0 + i*delta_i(n);
+//}; // Coordinate of the i-th node in the reference cell.
+
+template <ushort... dimensions> 
+static inline constexpr 
+std::array<double, sizeof...(dimensions)> 
+xi(std::array<double, sizeof...(dimensions)> index)
+{ 
+  constexpr std::size_t Dim =  sizeof...(dimensions);
+  std::array<double,Dim> coordinates{dimensions...};
+
+  for(int position = 0; position<Dim; ++position){
+    coordinates[position] = -1 + position*delta_i(coordinates[position]);
+  };
+
+  return coordinates;
+};
+
+//template <ushort Dim, ushort... n>
+//static inline constexpr Point<Dim> node_ijk(auto... index) {
+//  static_assert(sizeof...(n) == sizeof...(index),
+//                "Number of indices must match the number of template "
+//                "parameters (minus one)");
+//  static_assert(sizeof...(index) == Dim,
+//                "Number of indices must match the dimension of the cell");
+//  return Point<Dim>( xi<n>(index)... );
+//};
 
 template <ushort Dim, ushort... n>
-static inline constexpr Point<Dim> cell_ijk(auto... index) {
-  static_assert(sizeof...(n) == sizeof...(index),
-                "Number of indices must match the number of template "
-                "parameters (minus one)");
-  static_assert(sizeof...(index) == Dim,
-                "Number of indices must match the dimension of the cell");
-  return Point<Dim>(xi<n>(index)...);
+static inline constexpr Point<Dim> node_ijk(std::array<uint,Dim> ijk) {
+  std::array<double,Dim> coordinates;
+  return Point<Dim>(xi<n ...>(coordinates));
 };
 
 
-template<ushort Dim, ushort... n> requires(sizeof...(n) == Dim)
-static constexpr inline auto index_2_ijk(auto i){
-    
-  constexpr uint nx_ny_nz[Dim]{n...};
-  constexpr uint ijk[Dim];
+template<ushort Dim, ushort... n> requires(sizeof...(n) == Dim) // Dim se debe poder extraer del numero de parámetros.
+static constexpr inline std::array<uint, Dim> index_2_ijk(auto list_index){
+  constexpr std::array<uint, Dim> nx_ny_nz{n...}; //Number of nodes per direction.
   
-  for (auto d=0; d<Dim; ++d){
-    ijk[d] = i % nx_ny_nz[d];
-    i /= nx_ny_nz[d];
-  }
+  std::array<uint, Dim> ijk;
+  ijk[0] = 0; //nx_ny_nz[0]%list_index==0?  nx_ny_nz[0] : list_index%nx_ny_nz[0];
+  if constexpr(Dim>1){
+    ijk[1] = 0 ;//(list_index-ijk[0])/nx_ny_nz[0] + 1;
+    if constexpr(Dim>2){
+      ijk[2] = 0;
+    };
+  };
   return ijk;
 }
 
 template<ushort Dim, ushort... n> requires(sizeof...(n) == Dim)
-static consteval std::array<Point<Dim>, n_nodes_<Dim, n...>()> cell_nodes() {
-  constexpr std::array<Point<Dim>, n_nodes_<Dim, n...>()> nodes;
+consteval std::array<Point<Dim>, n_nodes_<Dim, n...>()> cell_nodes(){
+  constexpr std::size_t num_nodes = n_nodes_<Dim, n...>();
 
-  for (auto i = 0; i < n_nodes_<Dim, n...>(); ++i) {
-    nodes[i] = cell_ijk<Dim, n...>(index_2_ijk<Dim,n...>(i));
+  std::array<Point<Dim>, num_nodes> nodes;
+
+  for (auto i = 0; i < num_nodes; ++i) 
+  {
+    nodes[i] = node_ijk<Dim, n...>( index_2_ijk<Dim,n...>(i));
   }
   return nodes;
 };
 
+
+
+// ==================================================================================================
 
 template <ushort Dim, ushort... n> // n: Number of nodes per direction.
   requires(sizeof...(n) == Dim || sizeof...(n) == 1)
@@ -102,14 +134,13 @@ private:
 
 public:
   static constexpr uint n_nodes = n_nodes_<Dim, n...>();
-
   static constexpr std::array<Point, n_nodes> reference_nodes{cell_nodes<Dim, n...>()};
 
   consteval Cell() {
     if constexpr (sizeof...(n) == 1) {
       nodes_per_direction.fill(n...);
     }
-  
+    
   };
 
   constexpr ~Cell(){};
