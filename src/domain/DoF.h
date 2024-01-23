d1a348b230aee44627e45e225d3c98b17476bb07
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-01-22 22:49:37 -0500

    setting DoF_Interfase and DoF_Handler clases.

--- src/domain/DoF.h
+++ src/domain/DoF.h
@@ -1,26 +1,92 @@
-#ifndef FALL_N_DEGGREE_OF_FREEDOM_CONTAINER  
+#ifndef FALL_N_DEGGREE_OF_FREEDOM_CONTAINER
 #define FALL_N_DEGGREE_OF_FREEDOM_CONTAINER
 
-
+#include <array>
+#include <cstddef>
+#include <initializer_list>
+#include <memory>
 #include <utility>
-#include "../numerics/Vector.h"
+#include <vector>
+#include <iostream>
+#include <optional>
+#include <span>
+// #include "../numerics/Vector.h"
 
-namespace domain{ 
 
-  template<unsigned int nDoF> 
-  class DofContainer: public Vector<nDoF>{
+// https://stackoverflow.com/questions/39288891/why-is-shared-ptrvoid-legal-while-unique-ptrvoid-is-ill-formed 
+//using unique_void_ptr = std::unique_ptr<void, void(*)(void const*)>;
+//
+//template<typename T>
+//auto unique_void(T* ptr) -> unique_void_ptr
+//{
+//    return unique_void_ptr(ptr, [](void const* data) {
+//         T const* p = static_cast<T const*>(data);
+//         delete p;
+//    });
+//}
+//
+//template<typename T, typename... Args>
+//auto make_unique_void(Args&&... args)
+//{
+//     return unique_void(new T(std::forward<Args>(args)...));
+//}
+// ============================================================================================================
 
-    // More things to implement...
-    // unsigned int id = 0;
-    // map
-    // new id
-    public:
 
-      //template <typename... Args>
-      //DofContainer(Args&&... args):Eigen::Matrix<double, nDoF, 1>(std::forward<Args>(args)...)
-      //{};
+namespace domain {
+
+//template <std::size_t nDoF> 
+class DoF_Handler {
+
+  std::vector<std::size_t> dof_index_{}; //veiw?
+public:
+
+  void set_dof_index(std::initializer_list<std::size_t> dofs){
+    std::move(dofs.begin(), dofs.end(), dof_index_.begin());
   };
 
-}
+  DoF_Handler() = default;
+
+  DoF_Handler(std::initializer_list<std::size_t> dofs){
+    dof_index_.reserve(dofs.size());
+    std::move(dofs.begin(), dofs.end(), dof_index_.begin());
+  };
+};
+
+
+class DoF_Interfase{
+  
+  private:
+    //class DoF_Handler;
+    std::shared_ptr<DoF_Handler> dof_handler_ ;
+
+  public:
+
+    //void set_handler(DoF_Handler&& handler){
+    //  dof_handler_ = std::make_shared<DoF_Handler>(std::forward<DoF_Handler>(handler));
+    //};
+
+    void set_handler(std::initializer_list<std::size_t>&& dofs){
+      dof_handler_ = std::make_shared<DoF_Handler>(std::forward<std::initializer_list<std::size_t>>(dofs));
+    };
+
+
+    DoF_Interfase() = default;
+    ~DoF_Interfase(){};
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+} // namespace domain
 
 #endif
