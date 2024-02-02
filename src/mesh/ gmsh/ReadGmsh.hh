dd3d4ad5774532b0683b78206cc0c4321b7b8964
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-02-02 10:23:01 -0500

    Refactor code: Remove unused includes and delete ReadGmsh.hh

--- src/mesh/ gmsh/ReadGmsh.hh
+++
@@ -1,9 +0,0 @@
-#ifndef FALL_N_READ_GMSH
-#define FALL_N_READ_GMSH
-
-#include "../Mesh.hh"
-#include "../../domain/Domain.hh"
-
-
-
-#endif // FALL_N_MESH_INTERFACE
---
+++ src/model/Model.hh
@@ -0,0 +1,15 @@
+#ifndef FALL_N_MODEL_HH
+#define FALL_N_MODEL_HH
+
+class Model {
+public:
+    // Constructor
+    Model();
+
+    // Destructor
+    ~Model();
+
+    // Other members
+};
+
+#endif // FALL_N_MODEL_HH
