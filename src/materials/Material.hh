ef3586e406c5eb89b66d7bec24cf55afdd91a4d0
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-02-08 06:40:41 -0500

    Setting Material Class as type erased wrapper

--- src/materials/Material.hh
+++ src/materials/Material.hh
@@ -3,28 +3,73 @@
 
 
 #include <Eigen/Dense>
+#include <asm-generic/errno.h>
+#include <memory>
+#include <random>
 #include <vector>
 
 #include "../numerics/Tensor.hh"
 #include "Strain.hh"
 
-typedef unsigned short ushort;
-typedef unsigned int   uint  ;
 
+
+namespace impl { //implementation details
+
+class MaterialConcept 
+{
+    public:
+    virtual ~MaterialConcept() = default;
+
+    virtual void get_stress() const = 0;    // get cause 
+    //virtual void update_strain() const = 0; // update effect
+    
+     // virtual void commit_state() const = 0; // From OpenSees Material  
+
+    virtual std::unique_ptr<MaterialConcept> clone() const = 0; 
+};
+
+template<typename MaterialType, typename MaterialPolicy>
+class OwningMaterialModel : public MaterialConcept{
+
+    MaterialType   material_;
+    MaterialPolicy policy_  ; // Uniaxial, Continuum, Hysteretic, Force-Displacement...
+
+  public:
+    std::unique_ptr<MaterialConcept> clone() const override{
+        return std::make_unique<OwningMaterialModel<MaterialType,MaterialPolicy>>(*this);}; 
+
+    explicit OwningMaterialModel(MaterialType mat , MaterialPolicy pol) :
+        material_(std::move(mat)),
+        policy_  (std::move(pol)){};
+
+  public: //Implementations
+
+  void get_stress(){material_.get_stress();};      
+};
+
+}
 //template<ushort Dim, ushort Order> // Dim?
 class Material{
+    std::unique_ptr<impl::MaterialConcept> pimpl_; //Pointer to implementation details 
 
+  public:
+    template<typename MaterialType, typename MaterialPolicy>
+    Material (MaterialType mat_, MaterialPolicy pol_)
+    {
+        using Model = impl::OwningMaterialModel<MaterialType,MaterialPolicy>;
+
+        pimpl_ = std::make_unique<Model>(
+            std::move(mat_),
+            std::move(pol_));
+    }
+
+    ~Material() = default;
     // nVars: Number of state variables (strains)
-    private:
-        
+    //private:        
         //Tensor<Dim,Order> strain;
         //std::vector<Tensor<Dim,Order>> strain_t_; //Store history of strains in simulation for memory materials
-
-
-    public:
-        void preallocate_strain_t_(){};
-    
-        
+    //public:
+    //    void preallocate_strain_t_(){};
 
 };
 
