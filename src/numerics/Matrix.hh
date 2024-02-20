85b13c5f7bc99a61322a072711deae9c39f14ace
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-02-19 21:36:15 -0500

    Add matrix operations and print statements

--- src/numerics/Matrix.hh
+++ src/numerics/Matrix.hh
@@ -16,66 +16,115 @@
 
 
 
+#include "Vector.hh"
+
+
+
 class Matrix // Wrapper Around PETSc DenseMatrix
 {
+    using PETSc_DENSE_SEQ_Matrix = Mat;
+    
     private:
-        Mat mat_;
+    PETSc_DENSE_SEQ_Matrix mat_;
+
+    
     public:
 
-        void print_content(){
-            MatView(mat_, PETSC_VIEWER_STDOUT_SELF);
-        };
+    void print_content(){MatView(mat_, PETSC_VIEWER_STDOUT_SELF);};
+    //Operators
+    Matrix& operator+=(const Matrix& other){
+        MatAXPY(mat_, 1.0, other.mat_, SAME_NONZERO_PATTERN);
+        return *this;
+    };
+    Matrix operator-= (const Matrix& other){
+        MatAXPY(mat_, -1.0, other.mat_, SAME_NONZERO_PATTERN);
+        return *this;
+    };
+    Matrix operator*=(const PetscScalar& scalar){
+        MatScale(mat_, scalar);
+        return *this;
+    };
+    Matrix operator/=(const PetscScalar& scalar){
+        MatScale(mat_, 1.0/scalar);
+        return *this;
+    };
 
-        Matrix(PetscInt rows, PetscInt cols){
-            MatCreateSeqDense(PETSC_COMM_SELF, rows, cols, nullptr, &mat_);
-        };
+    Matrix operator*(const PetscScalar& scalar){
+        Mat B;
+        MatDuplicate(mat_, MAT_COPY_VALUES, &B);
+        MatScale(B, scalar);
+        return Matrix(B);
+    };
 
-        Matrix(PetscScalar* data, PetscInt rows, PetscInt cols){
-            MatCreateSeqDense(PETSC_COMM_SELF, rows, cols, data, &mat_);
-        };
+    Matrix operator/(const PetscScalar& scalar){
+        Mat B;
+        MatDuplicate(mat_, MAT_COPY_VALUES, &B);
+        MatScale(B, 1.0/scalar);
+        return Matrix(B);
+    };
+
+    Matrix operator+(const Matrix& other){
+        Mat B;
+        MatDuplicate(mat_, MAT_COPY_VALUES, &B);
+        MatAXPY(B, 1.0, other.mat_, SAME_NONZERO_PATTERN);  
+        return Matrix(B);
+    };
+
+    Matrix operator-(const Matrix& other){
+        Mat B;
+        MatDuplicate(mat_, MAT_COPY_VALUES, &B);
+        MatAXPY(B, -1.0, other.mat_, SAME_NONZERO_PATTERN);
+        return Matrix(B);
+    };
 
 
-        Matrix(std::ranges::contiguous_range auto& data, PetscInt extent1, PetscInt extent2){
-            MatCreateSeqDense(PETSC_COMM_SELF, extent1, extent2, std::to_address(data.begin()), &mat_);
-        };
-        
 
-        //auto data(){
-        //    PetscInt rows, cols;
-        //    MatGetSize(mat_, &rows, &cols);
-        //    PetscScalar* p_data;
-        //    MatGetArray(mat_, &p_data);
-        //    return std::span<PetscScalar>(p_data, rows*cols);
-        //}
+        //Constructors
 
-        //void print_content(){
-        //    PetscInt rows, cols;
-        //    MatGetSize(mat_, &rows, &cols);
-        //    for(auto i:data()) printf("%f ", i);printf("\n");
-        //};
+    Matrix(PETSc_DENSE_SEQ_Matrix mat): mat_(std::move(mat)){};
 
-        //Operators
-        /*
-        Matrix& operator+=(const Matrix& other){
-            MatAXPY(mat_, 1.0, other.mat_, DIFFERENT_NONZERO_PATTERN);
-            return *this;
-        };
+    Matrix(PetscInt rows, PetscInt cols){
+        MatCreateSeqDense(PETSC_COMM_SELF, rows, cols, nullptr, &mat_);
+    };
+    
+    Matrix(PetscScalar* data, PetscInt rows, PetscInt cols){
+        MatCreateSeqDense(PETSC_COMM_SELF, rows, cols, data, &mat_);
+    };
+    
+    Matrix(std::ranges::contiguous_range auto const& data, PetscInt extent1, PetscInt extent2){
+        MatCreateSeqDense(PETSC_COMM_SELF, extent1, extent2, std::to_address(data.begin()), &mat_);
+    };
+    
+    Matrix(std::ranges::contiguous_range auto&& data, PetscInt extent1, PetscInt extent2){
+        MatCreateSeqDense(PETSC_COMM_SELF, extent1, extent2, std::to_address(data.begin()), &mat_);
+    };
+    
+    //Copy Constructor
+    Matrix(const Matrix& other){
+        MatDuplicate(other.mat_, MAT_COPY_VALUES, &mat_);
+    };
+    
+    //Move Constructor
+    Matrix(Matrix&& other){
+        mat_ = other.mat_;
+        other.mat_ = nullptr;
+    };
+    
+    //Copy Assignment
+    Matrix& operator=(const Matrix& other){
+        MatDuplicate(other.mat_, MAT_COPY_VALUES, &mat_);
+        return *this;
+    };
+    
+    //Move Assignment
+    Matrix& operator=(Matrix&& other){
+        mat_ = other.mat_;
+        other.mat_ = nullptr;
+        return *this;
+    };
 
-        Matrix operator-= (const Matrix& other){
-            MatAXPY(mat_, -1.0, other.mat_, DIFFERENT_NONZERO_PATTERN);
-            return *this;
-        };
 
-        Matrix operator*=(const PetscScalar& scalar){
-            MatScale(mat_, scalar);
-            return *this;
-        };
 
-        Matrix operator/=(const PetscScalar& scalar){
-            MatScale(mat_, 1.0/scalar);
-            return *this;
-        };
-        */
     ~Matrix(){MatDestroy(&mat_);};
 };
 
