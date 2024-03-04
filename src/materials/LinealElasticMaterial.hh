a9a5122efc07dd9350348a1b824c9e7693bdbebd
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-03-03 21:40:40 -0500

    Refactor IsotropicElasticMaterial constructor

--- src/materials/LinealElasticMaterial.hh
+++ src/materials/LinealElasticMaterial.hh
@@ -15,14 +15,12 @@ class LinealElasticMaterial{};
 
 template<typename StrainType, typename ConstitutiveLaw>
 class IsotropicElasticMaterial{
-    using State           = ElasticMaterialState<StrainType>;
-    //using ConstitutiveLaw = ContinuumIsotropicRelation      ; // ERROR:
+    using State = ElasticMaterialState<StrainType>;
 
     std::unique_ptr<State> state_;
     std::shared_ptr<ConstitutiveLaw> constitutive_law_;
 
-    public:
-
+  public:
     auto get_state() const{return state_->current_state();};
     
     template<typename... Args>  
@@ -33,29 +31,9 @@ class IsotropicElasticMaterial{
 
     template<std::floating_point... Args>
     IsotropicElasticMaterial(Args... args) : 
-        state_{
-            std::make_unique<State>()
-            },
-        constitutive_law_{
-            std::make_shared<ConstitutiveLaw>(
-                std::forward<Args>(args)...
-            )
-            }
+        state_{std::make_unique<State>()},
+        constitutive_law_{std::make_shared<ConstitutiveLaw>(td::forward<Args>(args)...)}
         {};
-
-    /*
-    IsotropicElasticMaterial(double young_modulus, double poisson_ratio) : 
-        state_{
-            std::make_unique<State>()
-            },
-        constitutive_law_{
-            std::make_shared<ConstitutiveLaw>(
-                std::forward<double>(young_modulus),
-                std::forward<double>(poisson_ratio)
-                )
-            }
-        {};
-    */
 
 };
 
