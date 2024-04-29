157fcef7d03ffe65d89f5574e66a554b50938221
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-04-28 21:42:33 -0500

    Add MSHReader class and implement view functions

--- src/mesh/gmsh/ReadGmsh.hh
+++ src/mesh/gmsh/ReadGmsh.hh
@@ -4,6 +4,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <string_view>
+#include <charconv>
 
 #include <array>
 #include <map>
@@ -21,62 +23,104 @@ class MSHReader {
 
 // https://stackoverflow.com/questions/71882752/c-constexpr-stdarray-of-string-literals
 static constexpr std::array msh_keywords{
-        "$MeshFormat"            , //required always
-        "$EndMeshFormat"         , //required always
-        "$PhysicalNames"         ,
-        "$EndPhysicalNames"      ,
-        "$Entities"              , 
-        "$PartitionedEntities"   , 
-        "$EndPartitionedEntities",
-        "$Nodes"                 , //required always
-        "$EndNodes"              , //required always
-        "$Elements"              , //required always
-        "$EndElements"           , //required always
-        "$Periodic"              ,
-        "$EndPeriodic"           ,
-        "$GhostElements"         ,
-        "$EndGhostElements"      ,
-        "$Parametrizations"      ,
-        "$EndParametrizations"   ,
-        "$NodeData"              ,
-        "$EndNodeData"           ,
-        "$ElementData"           ,
-        "$EndElementData"        ,
-        "$ElementNodeData"       ,
-        "$EndElementNodeData"    ,
-        "$InterpolationScheme"   ,
-        "$EndInterpolationScheme" 
+        "$MeshFormat"            ,// 0  //required always
+        "$EndMeshFormat"         ,// 1  //required always
+        "$PhysicalNames"         ,// 2 
+        "$EndPhysicalNames"      ,// 3 
+        "$Entities"              ,// 4
+        "$EndEntities"           ,// 5 
+        "$PartitionedEntities"   ,// 6 
+        "$EndPartitionedEntities",// 7 
+        "$Nodes"                 ,// 8  //required always
+        "$EndNodes"              ,// 9  //required always
+        "$Elements"              ,// 10 //required always
+        "$EndElements"           ,// 11 //required always
+        "$Periodic"              ,// 12 
+        "$EndPeriodic"           ,// 13
+        "$GhostElements"         ,// 14
+        "$EndGhostElements"      ,// 15
+        "$Parametrizations"      ,// 16
+        "$EndParametrizations"   ,// 17
+        "$NodeData"              ,// 18
+        "$EndNodeData"           ,// 19
+        "$ElementData"           ,// 20
+        "$EndElementData"        ,// 21
+        "$ElementNodeData"       ,// 22
+        "$EndElementNodeData"    ,// 23
+        "$InterpolationScheme"   ,// 24
+        "$EndInterpolationScheme" // 25
     };
 
-std::ifstream file_;
+std::array<std::size_t, msh_keywords.size()> keyword_position_{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
 
 std::string filename_;
 std::string buffer_  ;
 
-
+std::string_view buffer_view_;
 
 public:
 
-MSHReader(std::string filename): filename_(filename){
-    file_.open(filename_);
-    if(!file_.is_open()){
-        std::cerr << "Error opening file: " << filename_ << std::endl;
+template<std::size_t N>
+inline constexpr auto view_keyword_info(){
+    std::size_t pos = keyword_position_[N];
+    std::size_t end = keyword_position_[N+1];
+
+    std::size_t count = end - pos;
+
+    if (end == 0){
+        std::cout << "Keyword " << msh_keywords[N] << " not found" << std::endl;
+        return std::string_view{};
     }
-    file_.seekg(0, std::ios::end);
-    buffer_.resize(file_.tellg());
-    file_.seekg(0, std::ios::beg);
-    file_.read(buffer_.data(), buffer_.size());
-    file_.close();
+    return buffer_view_.substr(pos, count);
+};
+
+inline constexpr auto view_MeshFormat         (){return view_keyword_info<0 >();};
+inline constexpr auto view_PhysicalNames      (){return view_keyword_info<2 >();};
+inline constexpr auto view_Entities           (){return view_keyword_info<4 >();};
+inline constexpr auto view_PartitionedEntities(){return view_keyword_info<6 >();};
+inline constexpr auto view_Nodes              (){return view_keyword_info<8 >();};
+inline constexpr auto view_Elements           (){return view_keyword_info<10>();};
+inline constexpr auto view_Periodic           (){return view_keyword_info<12>();};
+inline constexpr auto view_GhostElements      (){return view_keyword_info<14>();};
+inline constexpr auto view_Parametrizations   (){return view_keyword_info<16>();};
+inline constexpr auto view_NodeData           (){return view_keyword_info<18>();};
+inline constexpr auto view_ElementData        (){return view_keyword_info<20>();};
+inline constexpr auto view_ElementNodeData    (){return view_keyword_info<22>();};
+inline constexpr auto view_InterpolationScheme(){return view_keyword_info<24>();};
+
+
+void print_positions(){
+    for (auto const& pos : keyword_position_){
+        std::cout << pos << std::endl;
+    }
+};
+
+MSHReader(std::string_view filename): filename_(filename){
     
-    for (auto const& keyword : msh_keywords){
-        std::size_t pos = buffer_.find(keyword);
+    //https://stackoverflow.com/questions/18398167/how-to-copy-a-txt-file-to-a-char-array-in-c
+    std::ifstream in(filename_);
+    if (!in) {std::cerr << "Cannot open the File : " << filename_ << std::endl;}
+
+    buffer_ = std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+
+    std::size_t i = 0;
+    for (auto const& keyword : msh_keywords){ //sintetizar en función. set_positions();
+        std::size_t pos = buffer_.find(keyword); //Se puede hacer mas efiiente aprovechando el orden de los keywords.
         if (pos != std::string::npos){
+            keyword_position_[i] = pos;
             std::cout << "Found " << keyword << " at position " << pos << std::endl;
         }
+        ++i;
+
+        
     }
 
+    buffer_view_ = std::string_view(buffer_.data());
     
 }; 
+
+
 
 
 };  
