4af620f102b8911e362a53d69a6a01a4fe4e373d
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-06-21 17:36:21 -0500

    Typo fixed

--- src/mesh/gmsh/GmshDomainBuilder.hh
+++ src/mesh/gmsh/GmshDomainBuilder.hh
@@ -13,104 +13,83 @@
 #include "ReadGmsh.hh"
 #include "GmshElementTypes.hh"
 
-
 class GmshDomainBuilder_3D
 {
     using Node3D = Node<3>;
     gmsh::MSHReader mesh_info_;
 
-  public:
-
+public:
     std::vector<Node3D> nodes_;
     std::vector<Element> elements_;
 
-    void process_nodes(){
-        for (auto& block : mesh_info_.nodes_info_.entityBlocks){
-            for (std::size_t node = 0;  node < block.numNodesInBlock; node++){
+    void process_nodes()
+    {
+        for (auto &block : mesh_info_.nodes_info_.entityBlocks)
+        {
+            for (std::size_t node = 0; node < block.numNodesInBlock; node++)
+            {
                 nodes_.emplace_back(
                     Node3D(
                         std::move(block.nodeTag[node]),
                         std::move(block.coordinates[node][0]),
                         std::move(block.coordinates[node][1]),
-                        std::move(block.coordinates[node][2]) 
-                    )
-                );
+                        std::move(block.coordinates[node][2])));
             }
         }
     };
 
-/*
-    auto get_element_topology(std::map<std::size_t, std::vector<std::size_t>>& elementTags)
-    {
-        struct ElementTopology
-        {
-            std::size_t num_nodes;
-            std::string element_type;
-        };
-    };
-*/
-
-    void procces_elements(){ //Esto procesa tanto elemento 2d (facets) como 3d  (cells) TODO:FIX. 
-        
-        //Reserve space for elements
+    void process_elements()
+    { // Esto procesa tanto elemento 2d (facets) como 3d  (cells) TODO:FIX.
         elements_.reserve(mesh_info_.elements_info_.numElements);
 
-        auto default_integration_scheme = [](auto const & e){/**integrate*/};
+        auto default_integration_scheme = [](auto const &e) { /**integrate*/ };
 
-        for (auto& block : mesh_info_.elements_info_.entityBlocks){
-
-            if (block.entityDim == 3){//Only 3D elements (by now...)
-                for(auto& [element_tag, node_tags] : block.elementTags){
-                    
-                    std::vector<Node3D*> element_node_pointers;
+        for (auto &block : mesh_info_.elements_info_.entityBlocks)
+        {
+            if (block.entityDim == 3)
+            { // Only 3D elements (by now...)
+                for (auto &[element_tag, node_tags] : block.elementTags)
+                {
+                    std::vector<Node3D *> element_node_pointers;
                     element_node_pointers.reserve(node_tags.size());
 
-                    for (auto& tag : node_tags){
-
-                        if(nodes_[tag-1].id() == tag)
+                    for (auto &tag : node_tags)
+                    {
+                        if (nodes_[tag - 1].id() == tag)
                         {
-                            element_node_pointers.push_back(&nodes_[tag-1]);
+                            element_node_pointers.push_back(&nodes_[tag - 1]);
                         }
                         else
-                        {  //TODO: search for the node with the current tag.
+                        { // TODO: search for the node with the current tag.
                             throw std::runtime_error("Node tag does not match with node id");
                         }
                     }
-                // Emplace Element
-                switch (block.elementType)
-                {
-                case 5 :
-                    elements_.emplace_back(
-                        Element(
-                            LagrangeElement<2,2,2>{
-                                std::move(element_tag),
-                                std::move(element_node_pointers)
-                            },
-                            default_integration_scheme
-                        )
-                    );
-                    std::cout << "Hi!" << std::endl;
-                    break;
-                
-                default:
-                    throw std::runtime_error("Element type not supported");
-                    break;
-                }
-                }
-            }            
+                    // Emplace Element
+                    switch (block.elementType)
+                    {
+                    case 5:
+                        elements_.emplace_back(
+                            Element(
+                                LagrangeElement<2, 2, 2>{
+                                    std::move(element_tag),
+                                    std::move(element_node_pointers)},
+                                default_integration_scheme));
+                        break;
 
+                    default:
+                        throw std::runtime_error("Element type not supported");
+                        break;
+                    }
+                }
+            }
         }
     };
 
     GmshDomainBuilder_3D(std::string_view filename) : mesh_info_(filename)
     {
         process_nodes();
-        procces_elements();
+        process_elements();
     };
-
 };
-
-
-
 
 #endif // GMSH_DOMAIN_BUILDER_HH
