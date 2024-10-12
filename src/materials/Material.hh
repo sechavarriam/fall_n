356b2fefa7daa3579f992fd21041dfbb4dc4cf29
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-10-11 23:30:12 -0500

    Material Wrapper Working in simple version (NonOwning version commented)

--- src/materials/Material.hh
+++ src/materials/Material.hh
@@ -18,6 +18,103 @@
 
 #include "update_strategy/lineal/ElasticUpdate.hh"
 
+
+// Simple Type Erausre
+
+namespace impl{
+   template<class MaterialPolicy>
+   class MaterialConcept{ 
+      using StateVariableT = MaterialPolicy::StateVariableT;
+      using StressT        = MaterialPolicy::StressType;
+
+   public:
+      virtual constexpr ~MaterialConcept() = default;
+      virtual constexpr std::unique_ptr<MaterialConcept> clone() const = 0; // The Prototype design pattern
+
+   
+   public:
+      virtual constexpr StateVariableT get_state() const = 0; //The current Value of the State Variable (or the head?)
+      virtual void update_state(StateVariableT& state) = 0;  
+
+   };
+
+   template <typename MaterialType, typename UpdateStrategy>
+   class OwningMaterialModel : public MaterialConcept<typename MaterialType::MaterialPolicy>
+   {
+   private:
+
+      using MaterialPolicy = typename MaterialType::MaterialPolicy;
+      using StateVariableT = MaterialPolicy::StateVariableT;
+
+      MaterialType   material_        ;
+      UpdateStrategy update_algorithm_; //or material_integrator
+
+   public:
+      
+      StateVariableT get_state() const override {return material_.get_state();}; //CurrentValue
+
+      void update_state(StateVariableT& state) override {material_.update_state(state);};
+
+      explicit OwningMaterialModel(MaterialType material, UpdateStrategy material_integrator): 
+          material_        {std::move(material)}, 
+          update_algorithm_{std::move(material_integrator)}
+      {}
+
+      std::unique_ptr<MaterialConcept<MaterialPolicy>> clone() const override{ // The Prototype design pattern
+         return std::make_unique<OwningMaterialModel>(*this);
+      }
+
+   }; // OwningMaterialModel
+
+} // namespace impl
+
+template<class MaterialPolicy>
+class Material
+{
+   using StateVariableT = MaterialPolicy::StateVariableT;
+
+   std::unique_ptr<impl::MaterialConcept<MaterialPolicy>> pimpl_; // The Bridge design pattern
+
+public:
+
+   StateVariableT get_state() const {return pimpl_->get_state();};
+   void update_state(StateVariableT& state) {pimpl_->update_state(state);};
+
+public:
+   
+   template <typename MaterialType, typename UpdateStrategy>
+   Material(MaterialType material, UpdateStrategy material_integrator){
+      using Model = impl::OwningMaterialModel<MaterialType, UpdateStrategy>;
+      pimpl_ = std::make_unique<Model>(
+         std::move(material), 
+         std::move(material_integrator)
+         );
+   }
+
+   Material(Material<MaterialPolicy> const &other) : pimpl_(other.pimpl_->clone()){}
+
+   Material &operator=(Material const &other)
+   {
+      // Copy-and-Swap Idiom
+      Material copy(other);
+      pimpl_.swap(copy.pimpl_);
+      return *this;
+   }
+
+   ~Material() = default;
+   Material(Material &&) = default;
+   Material &operator=(Material &&) = default;
+
+private: //Hidden Friends (Free Functions)
+
+
+};
+
+
+/*
+
+
+// Non-owning Type Erasure V2
 namespace impl //Implementation Details
 {
    template<class MaterialPolicy>
@@ -27,8 +124,9 @@ namespace impl //Implementation Details
 
    public:
       virtual constexpr ~MaterialConcept() = default;
-      virtual constexpr std::unique_ptr<MaterialConcept> clone() const = 0; // The Prototype design pattern
-      virtual constexpr void clone(MaterialConcept *address) const  = 0;      // The Prototype design pattern
+      virtual constexpr std::unique_ptr<MaterialConcept> clone() = 0; // The Prototype design pattern
+
+      virtual constexpr void clone(MaterialConcept *address) = 0;      
    
    public:
       virtual constexpr StateVariableT get_state() const = 0; //The current Value of the State Variable (or the head?)
@@ -62,15 +160,16 @@ namespace impl //Implementation Details
           update_algorithm_{std::move(material_integrator)}
       {}
 
-      std::unique_ptr<MaterialConcept<MaterialPolicy>> clone() const override{ // The Prototype design pattern
+      std::unique_ptr<MaterialConcept<MaterialPolicy>> clone() override{ // The Prototype design pattern
          return std::make_unique<OwningMaterialModel>(*this);
       }
 
-      void clone(MaterialConcept<MaterialPolicy>* address) const override{
-         using Model = NonOwningMaterialModel<MaterialType const, UpdateStrategy const>;
+      void clone(MaterialConcept<MaterialPolicy>* address) override{
+         using Model = NonOwningMaterialModel<MaterialType const, UpdateStrategy const>;   
          std::construct_at(static_cast<Model*>(address), material_, update_algorithm_);
+
       }
-   };
+   }; // OwningMaterialModel
 
    template <typename MaterialType, typename UpdateStrategy>
    class NonOwningMaterialModel : public MaterialConcept<typename MaterialType::MaterialPolicy>{
@@ -93,12 +192,12 @@ namespace impl //Implementation Details
            update_algorithm_{std::addressof(material_integrator)}
       {}
 
-      std::unique_ptr<MaterialConcept<MaterialPolicy>> clone() const override{
+      std::unique_ptr<MaterialConcept<MaterialPolicy>> clone() override{
          using Model = OwningMaterialModel<MaterialType, UpdateStrategy>;
          return std::make_unique<Model>(*material_, *update_algorithm_);
       }
 
-      void clone(MaterialConcept<MaterialPolicy> *address) const override{
+      void clone(MaterialConcept<MaterialPolicy> *address) override{
          std::construct_at(static_cast<NonOwningMaterialModel *>(address), *this);
       }
    };
@@ -130,8 +229,7 @@ public:
    // lvalue references prevent references to rvalues
    template <typename MaterialType, typename UpdateStrategy>
    MaterialConstRef(MaterialType &material, UpdateStrategy &material_integrator){
-      using Model =
-          impl::NonOwningMaterialModel<MaterialType const, UpdateStrategy const>;
+      using Model = impl::NonOwningMaterialModel<MaterialType const, UpdateStrategy const>;
       static_assert(sizeof(Model) == MODEL_SIZE, "Invalid size detected");
       static_assert(alignof(Model) == alignof(void *), "Misaligned detected");
 
@@ -164,7 +262,7 @@ private:
       return reinterpret_cast<impl::MaterialConcept<MaterialPolicy> const*>(raw_.data());
    }
 
-};
+}; // MaterialConstRef
 
 template<class MaterialPolicy>
 class Material
@@ -208,8 +306,7 @@ public:
 
 private: //Hidden Friends (Free Functions)
 
-
-};
+}; // Material
 
 template<class MaterialPolicy>
 inline MaterialConstRef<MaterialPolicy>::MaterialConstRef(Material<MaterialPolicy> &other){
@@ -221,7 +318,7 @@ inline MaterialConstRef<MaterialPolicy>::MaterialConstRef(Material<MaterialPolic
    other.pimpl_->clone(pimpl());
 }
 
-
+*/
 
 
 
