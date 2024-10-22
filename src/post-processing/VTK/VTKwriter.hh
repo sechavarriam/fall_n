a846b0ec5b2c3e45bbbf546c3f2e8a53b111b696
Sebastián Echavarría <sechavarriam@unal.edu.co>
2024-10-21 22:13:10 -0500

    New branch to implement Visualization in VTK

---
+++ src/post-processing/VTK/VTKwriter.hh
@@ -0,0 +1,13 @@
+#ifndef VTKWRITER_HH
+#define VTKWRITER_HH
+
+//#include <vtkSmartPointer.h>
+//#include <vtkXMLPolyDataWriter.h>
+//#include <vtkPolyData.h>
+
+// Your class and function declarations go here
+
+
+
+
+#endif // VTKWRITER_HH
