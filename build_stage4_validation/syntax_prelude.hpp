fcc773a470b944e13952cdb001a3d860b3309e0a
Sebastián Echavarría Montaña <sechavarriam@unal.edu.co>
2026-05-08 11:48:41 -0500

    Remove obsolete generated validation artifacts

--- build_stage4_validation/syntax_prelude.hpp
+++
@@ -1,7 +0,0 @@
-#define FALL_N_SOURCE_DIR "C:/MyLibs/fall_n"
-#define PETSC_FUNCTION_NAME_CXX __PRETTY_FUNCTION__
-#define PETSC_CXX_RESTRICT __restrict__
-#define VTK_AOS_DATA_ARRAY_TEMPLATE_EXTERN
-#define VTK_GDA_TEMPLATE_EXTERN
-#define kiss_fft_scalar double
-#define KISSFFT_DLL_IMPORT 1
